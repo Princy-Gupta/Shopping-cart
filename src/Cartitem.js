b95be93701916231762af51a976d71efce38fa06
Princy-Gupta <princy1894.cse18@chitkara.edu.in>
2021-05-09 12:10:44 +0530

    adding functions

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -3,6 +3,15 @@
 import React, { Component } from 'react'
 import './App.css'
 export default class  extends Component {
+    constructor()
+    {   super();
+        this.state={
+            price:10000,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
   render() {
     return (
       <div className="cart-item">
@@ -10,11 +19,14 @@ export default class  extends Component {
             <img style={styles.image}/>
         </div>
         <div className="right-block">
-            <div style={{fontSize:25}}>Phone</div>
-            <div style={{color:'#777'}}>Price:Rs 10,000 </div>
-            <div style={{color:'#777'}}>Qty:1</div>
+            <div style={{fontSize:25}}>{this.state.title}</div>
+            <div style={{color:'#777'}}>Price:{this.state.price} </div>
+            <div style={{color:'#777'}}>Qty:{this.state.qty}</div>
             <div className="cart-item-actions">
                 {/*buttons*/}
+                <img alt="increase" onClick={this.inc} className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1620542541~hmac=7ec6964672e0d7264b6f6dc56b7e166d"/>
+                <img alt="decrease" onClick={this.dec} className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1620542338~hmac=b0cc68f0b751ae0ca23ef5992d4dbe17"/>
+                <img alt="delete" onClick={this.del} className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1620542610~hmac=c5beeac097238ea43ddc8c5a79b115e9"/>
             </div>
         </div>
       </div>
@@ -24,8 +36,8 @@ export default class  extends Component {
 
 const styles={
     image:{
-      height:110,
-      width:110,
+      height:120,
+      width:120,
       borderRadius:4,
       background:'#ccc'
     }
